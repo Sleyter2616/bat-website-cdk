8d535494940ff439953bc7d0c5bd01220d878214
Sleyter2616 <62727289+Sleyter2616@users.noreply.github.com>
2023-11-15 11:29:11 -0500

    BATPLAT-702 (#1)
    Co-authored-by: Sleyter De La Cruz <Sleyter.delacruz@Hearst.com>

--- packages/backend/bin/backend.ts
+++ packages/backend/bin/backend.ts
@@ -20,11 +20,11 @@ const amplifyHostingStack = new AmplifyHostingStack(
   {
     // Name given to plaintext secret in secretsManager.
     // When creating the token scope on Github, only the admin:repo_hook scope is needed
-    githubOauthTokenName: "bat-monorepo-cdk-next-amplify-pat",
+    githubOauthTokenName: "staging/bat-website-github-pat-owen",
     // swap for your github username
-    owner: "chrismejia",
+    owner: "owen-cole",
     // swap for your github frontend repo
-    repository: "bat-monorepo-cdk-next-amplify",
+    repository: "bringatrailer-dev/bat-website",
     //pass in any envVars from the above stacks here
     environmentVariables: {
       USERPOOL_ID: authStack.userpool.userPoolId,
