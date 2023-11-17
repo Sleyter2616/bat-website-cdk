3bb1a3582022e67de4226f91c1046f37d8578add
Sleyter2616 <sleyter2616@gmail.com>
2023-11-17 11:28:11 -0500

    added amplify auth configuration to frontend code along with backend configuration to pick up on amplify

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../utils/aws/Amplify'
 import { Authenticator } from '@aws-amplify/ui-react';
 import type { AppProps } from 'next/app';
+import { Amplify } from 'aws-amplify';
+import config from '../amplifyconfiguration.json';
+Amplify.configure(config);
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
